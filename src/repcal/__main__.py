6bec9fbddb3a141101ae17471239f50de85befd8
Tomas Thelander <tomas.thelander0@gmail.com>
2020-10-20 11:22:26 +0200

    Option to convert spcific date/time from command line

--- src/repcal/__main__.py
+++ src/repcal/__main__.py
@@ -1,9 +1,19 @@
 from datetime import datetime
+import argparse
 from .RepublicanDate import RepublicanDate
 from .DecimalTime import DecimalTime
 
 
-t = datetime.utcnow()
+parser = argparse.ArgumentParser(description='Convert a date and time')
+parser.add_argument('date', help='The date to convert', nargs='?')
+
+args = parser.parse_args()
+
+if args.date == None:
+    t = datetime.utcnow()
+else:
+    t = datetime.fromisoformat(args.date)
+
 
 dtime = DecimalTime.from_standard_time(t.time())
 rdate = RepublicanDate.from_gregorian(t.date())
