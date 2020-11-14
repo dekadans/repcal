111242630c652f8fc608284009db49a32173ef81
Tomas Thelander <tomas.thelander0@gmail.com>
2020-11-14 15:15:27 +0100

    Directory structure

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dekadans/repcal",
+    licence='MIT',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
